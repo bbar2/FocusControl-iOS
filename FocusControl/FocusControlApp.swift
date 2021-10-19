498b0ff60bdbb7d21b70de8e4823d69390e13803
B Bryant <no_mail@b3sk.com>
2021-10-19 15:48:14 -0400

    Initial Commit

---
+++ FocusControl/FocusControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusControlApp.swift
+//  FocusControl
+//
+//  Created by Barry Bryant on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct FocusControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
