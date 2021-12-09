9d49ef61809384f770e5ca7f73371f4b123339e4
B Bryant <no_mail@b3sk.com>
2021-12-09 09:06:30 -0500

    First crude haptic cut.  It works, but not confident in the approach.

--- FocusControl/ControView.swift
+++ FocusControl/ControView.swift
@@ -15,19 +15,19 @@
 // FocusMotorController transmits FocusMotor commands via Bluetooth Low
 // Energy (BLE). UI status message field shows state of BLE connection.
 
-
 import SwiftUI
+import CoreHaptics
 
 struct ControlView: View {
   @ObservedObject var viewModel = ControlViewModel();
-  
-  /// <#Description#>
+  @State private var engine:CHHapticEngine?
+
   var body: some View {
     VStack {
       
       // App Title and BLE connection status area
       // Yellow circle emulates yellow LED on hardware focus control
-      Text("Focus Control")
+      Text("Focus Control").bold()
       HStack{
         Text("Status: ")
         Text(viewModel.statusString)
@@ -37,7 +37,7 @@ struct ControlView: View {
 
       // Focus mode selection and indication area
       // Red circles emulate red LEDs on hardware device.
-      Text("Focus Mode")
+      Text("Focus Mode").bold()
       HStack{
         Picker(selection: $viewModel.focusMode,
                label: Text("???")) {
@@ -47,20 +47,24 @@ struct ControlView: View {
         }
         .pickerStyle(.segmented)
         .colorMultiply(.red) // kludge to get red
+        .onTapGesture{hapticBump()}
       }
       
       Text("").padding(30)
 
       // Focus control area - BIG buttons simplify focusing
       // while looking through telescope and not at UI.
-      Text("Adjust Focus")
+      Text("Adjust Focus").bold()
       let frameWidth:CGFloat = 190
       HStack {
         Button("\nCounter\nClockwise\n") {
+          hapticBump()
           viewModel.updateMotorCommandCCW()}
         .frame(width:frameWidth)
         Button("\nClockwise\n\n") {
-          viewModel.updateMotorCommandCW()}
+          hapticBump()
+          viewModel.updateMotorCommandCW()
+        }
         .frame(width:frameWidth)
       }
       .buttonStyle(.bordered)
@@ -69,8 +73,9 @@ struct ControlView: View {
         Text(viewModel.motorCommand.description)
       }
     }
-    
+     
     .onAppear{
+      prepareHaptics()
       viewModel.focusMotorInit()
 
       //Change picker font size
@@ -84,7 +89,24 @@ struct ControlView: View {
     .controlSize(.large)
     .font(.title)
   }
+
+  func prepareHaptics() {
+    guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+    
+    do {
+      self.engine = try CHHapticEngine()
+      try engine?.start()
+    } catch {
+      print("There was an error creating Haptics engine: \(error.localizedDescription)")
+    }
+  }
+  
+  func hapticBump() {
+    let impact = UIImpactFeedbackGenerator(style: .heavy)
+    impact.impactOccurred()
+  }
 }
+
 
 
 struct MainView_Previews: PreviewProvider {
